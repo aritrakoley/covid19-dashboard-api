80fb6be7b2c80410bfb2acedb530a16ac689f31c
Aritra Koley <koley.aritra824@gmail.com>
2020-04-17 22:02:33 +0530

    heroku deploy #7

--- API.py
+++ API.py
@@ -3,7 +3,7 @@ from data_scraper import get_formatted_data
 
 app = Flask(__name__)
 
-@app.route('/getJson')
+@app.route('/')
 def get_json():
     return "<h1> Heroku App Test </h1>"
     # json_data = jsonify(get_formatted_data(0))
