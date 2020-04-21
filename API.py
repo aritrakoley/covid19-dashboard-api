dc7a8aede00610195e4d38f9ab1d1625dbad5b1c
Aritra Koley <koley.aritra824@gmail.com>
2020-04-21 15:45:03 +0530

    added cors

--- API.py
+++ API.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from data_scraper import get_formatted_data
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/getData')
 def get_data():
